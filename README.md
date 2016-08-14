0577763dc9e010447d50558c1cbc0b6ad2ee693c
Will <creaked@gmail.com>
2016-08-14 00:04:12 -0400

    add README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# checklist
+### simple jQuery checklist
+
+### Check it out with [htmlpreview](http://htmlpreview.github.io/?https://github.com/creaked/checklist/blob/master/index.html)
--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-	
 	$('#button').click(function() {
 		var i=0;
 		var toAdd = $('input[name=item]').val();
 		$('.list').append('<div class="item"><li>' + toAdd + '</li></div>');
-
 		console.log('Added: ' + toAdd);
 	});
-			$('.list').append('Click item to remove!')
+
 	$(document).on('click', '.item', function() {
 		$(this).remove();
 		console.log('Removed: ' + $('input[name=item]').val());
 	});
+
+	$('.list').append('Click item to remove!')
 });
